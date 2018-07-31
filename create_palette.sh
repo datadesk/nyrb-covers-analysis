1f212c0753f69041ccef17ab42cf4951f4cff0b5
Aida Ylanan <a1daylanan@ucla.edu>
2018-07-31 13:46:59 -0700

    draft of palette shell script

---
+++ create_palette.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Creating palettes"
+
+echo "Deleting existing histogram data"
+rm -rf ./palettes.txt
+
+echo "Creating color palettes"
+for filepath in `pwd`/quantized/*; do
+	FILENAME="$(echo "$filepath" | rev | cut -d "/" -f1 | rev)"
+	TITLE="$(echo $FILENAME | sed -r 's/(\.gif)//g')"
+	echo "Making a histogram for $TITLE"
+	echo $TITLE >> `pwd`/palettes.txt
+	# need to run imagemagick a couple of times to get the right histogram... 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
+	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -5 >> `pwd`/palettes.txt
+done
