d9c35ff35fea8694b427dfe76478ed1ab18c3fb9
Aida Ylanan <a1daylanan@ucla.edu>
2018-07-31 14:17:03 -0700

    better palette creation woo

--- create_palette.sh
+++ create_palette.sh
@@ -11,10 +11,10 @@ for filepath in `pwd`/quantized/*; do
 	echo "Making a histogram for $TITLE"
 	echo $TITLE >> `pwd`/palettes.txt
 	# need to run imagemagick a couple of times to get the right histogram... 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -1 
-	convert $filepath -format %c histogram:info:histogram_image.txt & sort -n histogram_image.txt | tail -5 >> `pwd`/palettes.txt
+	convert $filepath -format %c histogram:info:$TITLE.txt 
+	convert $filepath -format %c histogram:info:$TITLE.txt | sort -n $TITLE.txt | tail -1 | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/'
+	convert $filepath -format %c histogram:info:$TITLE.txt | sort -n $TITLE.txt | tail -1 | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/'
+	convert $filepath -format %c histogram:info:$TITLE.txt | sort -n $TITLE.txt | tail -1 | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/'
+	convert $filepath -format %c histogram:info:$TITLE.txt | sort -n $TITLE.txt | tail -1 | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/'
+	convert $filepath -format %c histogram:info:$TITLE.txt | sort -n $TITLE.txt | tail -5 | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/' >> `pwd`/palettes.txt
 done
