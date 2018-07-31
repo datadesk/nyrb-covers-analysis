53436dd73f1d6f81250b9f7cb9c7839e03cf029e
Aida Ylanan <a1daylanan@ucla.edu>
2018-07-30 17:46:23 -0700

    add covers and more notebook and magick minor changes

--- magick_codes.sh
+++ magick_codes.sh
@@ -36,12 +36,30 @@ cd ImageMagick-7.0.8-8
 
 # pictures with lots of different colors and not a lot of variation in shading 
 /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/youll-enjoy-it-when-you-get-there.jpg
-# pro-tip for this cover ^ quantize first!!!
+# pro-tip for this cover ^ quantize first!!! (-colors 5)
+
+# books with similar colors all around
+/home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/we-think-the-world-of-you-3.jpg
+/home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/voltaire-in-love.jpg
+
 
 # TEST BY COVER
 # YOULL ENJOY IT WHEN YOU GET THERE
 convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/youll-enjoy-it-when-you-get-there.jpg -format %c -depth 5 histogram:info:histogram_image.txt | sort -n histogram_image.txt | tail -5
-convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/youll-enjoy-it-when-you-get-there.jpg -dither Riemersma -colors 8 \
+convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/youll-enjoy-it-when-you-get-there.jpg -dither Riemersma -colors 5 \
 	colors_8_rm.gif | convert colors_8_rm.gif -format %c -depth 5 histogram:info:histogram_image.txt | sort \
 	-n histogram_image.txt | sort -n histogram_image.txt | tail -5 
 convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/youll-enjoy-it-when-you-get-there.jpg -dither Riemersma -colors 5 colors_8_rm.gif | display colors_8_rm.gif
+
+# WE THINK THE WORLD OF YOU
+convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/we-think-the-world-of-you-3.jpg -format %c -depth 5 histogram:info:histogram_image.txt | sort -n histogram_image.txt | tail -5
+convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/we-think-the-world-of-you-3.jpg -dither Riemersma -colors 5 \
+	colors_5_rm.gif | convert colors_5_rm.gif -format %c -depth 5 histogram:info:histogram_image.txt | sort \
+	-n histogram_image.txt | sort -n histogram_image.txt | tail -5 
+convert /home/aida/nyrb-colors-analysis/nyrb-colors-analysis/covers/we-think-the-world-of-you-3.jpg -dither Riemersma -colors 5 colors_5_rm.gif | display colors_5_rm.gif
+
+
+# MORE NOTES
+# quantized images give a greater sense of color variety in an image
+# unquanitzed images are more representative of dominant colors/the way colors take up a space
+# at the moment, leaning toward quantized images -- since they group similar colors together, they'll group more similar covers together by color, right?
